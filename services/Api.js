efa965d42014942b72e886c0cd9d89659d401998
Joao Vitor S. Rezende <jvsrpassos@gmail.com>
2021-07-15 22:31:07 -0300

    Api service

---
+++ services/Api.js
@@ -0,0 +1,24 @@
+
+export default function Api() {
+  const api = {};
+
+  api.get = async function (url) {
+    const res = await fetch(url, { method: 'GET' } );
+    return await res.json();    
+  };
+
+  api.post = async function (url, data) {
+    const res = await fetch(
+      url,
+      {
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' },
+        method: 'POST'
+      }
+    );
+
+    return await res.json();
+  };
+
+  return api;
+}
